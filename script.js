0ed85147f36799dd0c87a5e030f5407626bcfc4d
Jayawd456 <156908915+Jayawd456@users.noreply.github.com>
2025-02-15 19:51:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ async function analyzeData() {
     formData.append('symptoms', symptoms);
 
     try {
-        const response = await fetch('http://localhost:5000/predict', {
+        const response = await fetch('https://misswd-qwertymodel.hf.space/run/predict', {
             method: 'POST',
             body: formData
         });
